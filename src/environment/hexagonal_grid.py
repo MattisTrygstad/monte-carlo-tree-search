6286393d65513ae9a0d85b553dfcf17be33dd96d
Mattis <mattislt@sf-nett.no>
2021-03-13 18:03:46 +0100

    wip: Win condition

--- src/environment/hexagonal_grid.py
+++ src/environment/hexagonal_grid.py
@@ -1,5 +1,7 @@
 
 from copy import deepcopy
+import math
+import sys
 from matplotlib import pyplot as plt
 import numpy as np
 from abstract_classes.environment import Environment
@@ -56,8 +58,59 @@ class HexagonalGrid(Environment):
         return list(self.state.get_empty_nodes().keys())
 
     def check_win_condition(self) -> bool:
-        # TODO: implement win condition
-        return False
+        start_col = 0
+        goal_col = self.state.size - 1
+
+        nodes = self.state.get_player_one_nodes()
+        path = {}
+        for start_node in nodes.keys():
+            if start_node[1] != start_col:
+                continue
+
+            distance = {}
+            q = {}
+            path = {}
+            for n in nodes.keys():
+                distance[n] = math.inf
+                q[n] = math.inf
+                path[n] = None
+
+            distance[start_node] = 0
+
+            while q:
+                curr_node = min(q, key=q.get)
+
+                del q[curr_node]
+
+                for neighbor in self.state.neighbors:
+                    next_node = (curr_node[0] + neighbor[0], curr_node[1] + neighbor[1])
+                    if next_node not in nodes:
+                        continue
+
+                    new_distance = distance[curr_node] + 1
+                    if new_distance < distance[next_node]:
+                        distance[next_node] = new_distance
+                        path[next_node] = curr_node
+
+        print(path)
+
+        shortest_path = []
+        for (child, parent) in path.items():
+            if child[1] != goal_col:
+                continue
+
+            shortest_path.append(child)
+
+            while True:
+                print('test')
+                if not parent:
+                    break
+                child = path[parent]
+                shortest_path.append(child)
+                parent = child
+
+        shortest_path.reverse()
+        print(shortest_path)
 
     def get_state(self) -> UniversalState:
         universal_state = UniversalState()
@@ -94,11 +147,15 @@ class HexagonalGrid(Environment):
         self.ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
         plt.title('Peg Solitaire') """
 
-        """ self.ax.text(1, -1, 'Player 2', fontsize=15, color=Color.PLAYER_2.value)
-        self.ax.text(-1.2, -5.2, 'Player 2', fontsize=15, color=Color.PLAYER_2.value)
+        x1 = 1.2
+        x2 = -1.8
+        y1 = -0.8
+        y2 = -3.5
 
-        self.ax.text(-1.2, -1, 'Player 1', fontsize=15, color=Color.PLAYER_1.value)
-        self.ax.text(1, -5.2, 'Player 1', fontsize=15, color=Color.PLAYER_1.value) """
+        self.ax.text(x2, y1, 'Player 1', fontsize=15, color=Color.PLAYER_1.value)
+        self.ax.text(x2, y2, 'Player 2', fontsize=15, color=Color.PLAYER_2.value)
+        self.ax.text(x1, y1, 'Player 2', fontsize=15, color=Color.PLAYER_2.value)
+        self.ax.text(x1, y2, 'Player 1', fontsize=15, color=Color.PLAYER_1.value)
 
         self.fig.tight_layout()
         plt.show(block=block)
