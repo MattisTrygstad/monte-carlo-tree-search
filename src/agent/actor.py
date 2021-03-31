fix: Fix MCTS search and backprop

src/agent/actor.py
@@ -8,33 +8,37 @@ import torch.nn as nn
 from environment.hexagonal_grid import HexagonalGrid
 from environment.universal_action import UniversalAction
 from environment.universal_state import UniversalState
+from utils.instantiate import instantiate_activation_func, instantiate_optimizer
 
 
 class Actor(nn.Module):
-    def __init__(self, epochs: int, learning_rate: float, save_interval: int, input_neurons: int, nn_dimensions: list, activation_functions: list) -> None:
+    def __init__(self, epochs: int, learning_rate: float, save_interval: int, board_size: int, nn_dimensions: list, activation_functions: list, optimizer: str) -> None:
         super(Actor, self).__init__()
         self.epochs = epochs
         self.learning_rate = learning_rate
         self.save_interval = save_interval
 
         # Input layer
-        network_config = [nn.Linear(input_neurons, nn_dimensions[0])]
-        print(input_neurons, nn_dimensions[0])
+        network_config = [nn.Linear(board_size**2 + 1, nn_dimensions[0])]
         network_config.append(nn.Dropout(0.5))
-        network_config.append(nn.ReLU())
+        activation_function = instantiate_activation_func(activation_functions[0])
+        network_config.append(activation_function) if activation_function else None
 
         # Hidden layers
-        for layer_index in range(len(nn_dimensions) - 2):
-            print(nn_dimensions[layer_index], nn_dimensions[layer_index + 1])
+        for layer_index in range(len(nn_dimensions) - 1):
             network_config.append(nn.Linear(nn_dimensions[layer_index], nn_dimensions[layer_index + 1]))
 
+            activation_function = instantiate_activation_func(activation_functions[layer_index + 1])
+            network_config.append(activation_function) if activation_function else None
+
         # Output layer
-        print(nn_dimensions[-2], nn_dimensions[-1])
-        network_config.append(nn.Linear(nn_dimensions[-2], nn_dimensions[-1]))
+        network_config.append(nn.Linear(nn_dimensions[-1], board_size**2))
         network_config.append(nn.Softmax(-1))
         self.model = nn.Sequential(*network_config)
-        self.optimizer = torch.optim.Adagrad(list(self.model.parameters()), lr=self.learning_rate)
+        self.optimizer = instantiate_optimizer(optimizer, list(self.model.parameters()), self.learning_rate)
         self.loss_function = nn.BCELoss(reduction='mean')
+
+        print(self.model)
 
     def generate_action(self, state: UniversalState, legal_actions: list) -> UniversalAction:
         input = Actor.__to_tensor(state.to_numpy())

src/agent/mcts.py
@@ -3,7 +3,9 @@
 from copy import deepcopy
 from random import choice, choices
 import random
+import sys
 from typing import Tuple
+from unittest.mock import seal
 import numpy as np
 from agent.actor import Actor
 from agent.node import Node
@@ -15,35 +17,45 @@ from environment.universal_state import UniversalState
 
 class MonteCarloTree:
 
-    def __init__(self, state: UniversalState, actor: Actor, epsilon: float) -> None:
-        self.root = Node(None, None, Player.ONE)
+    def __init__(self, state: UniversalState, actor: Actor, epsilon: float, exploration_constant: float) -> None:
+        self.root = Node(None, Player.ONE)
         self.init_state = state
         self.env = HexagonalGrid(self.init_state, False)
         self.epsilon = epsilon
+        self.exploration_constant = exploration_constant
         self.actor = actor
 
     def reset(self, state: UniversalState):
         self.env.reset()
         self.init_state = state
-        self.root = Node(None, None, Player.ONE)
+        self.root = Node(None, Player.ONE)
 
     def set_root(self, action: UniversalAction, state: UniversalState):
         self.init_state = state
         self.env.reset(state)
 
-        # TODO: discard unused children in tree
         self.root = self.root.children[action.coordinates]
 
     @staticmethod
-    def get_children_visit_count(node: Node) -> dict:
-        # Key: node_coordinate, value: visit_count
-        return {key: child.visit_count for key, child in node.children.items()}
+    def get_action_distribution(node: Node) -> dict:
+        total_visits = node.visit_count
+
+        distribution = {key: traversals / total_visits for key, [traversals, _] in node.edges.items()}
+
+        return distribution
 
     def single_pass(self) -> None:
         node = self.tree_search()
-        player = self.env.get_player_turn()
         winner = self.evaluate_leaf()
-        self.backprop(node, player, winner)
+        self.backprop(node, winner)
+
+    def tree_policy(self, node: Node) -> UniversalAction:
+        player = self.env.get_player_turn()
+        actions = list(node.children.keys())
+
+        tree_policy_values = [node.evaluate_action(action, self.exploration_constant) for action in actions]
+        selected_action = actions[np.argmax(tree_policy_values) if player == Player.ONE else np.argmin(tree_policy_values)]
+        return UniversalAction(selected_action)
 
     def tree_search(self) -> Node:
         """
@@ -52,23 +64,21 @@ class MonteCarloTree:
         node = self.root
         self.env.reset(UniversalState(deepcopy(self.init_state.nodes)))
 
-        while len(node.children) != 0:
-            visit_counts = self.get_children_visit_count(node)
-
-            max_value = max(visit_counts.values())
-            max_keys = [k for k, v in visit_counts.items() if v == max_value]
-
-            chosen_key = choice(max_keys)
-            node = node.children[chosen_key]
-            action = UniversalAction(chosen_key)
+        while len(node.children) != 0 and self.env.check_win_condition():
+            action = self.tree_policy(node)
+            node.cached_action = action
             self.env.execute_action(action)
+            node: Node = node.children[action.coordinates]
 
-            if visit_counts[chosen_key] == 0:
+            if node.visit_count == 0:
                 return node
 
         if self.expand_node(node):
-            node: Node = choice(list(node.children.values()))
-            self.env.execute_action(node.previous_action)
+            #node: Node = choice(list(node.children.values()))
+            action = self.tree_policy(node)
+            node.cached_action = action
+            self.env.execute_action(action)
+            node: Node = node.children[action.coordinates]
 
         return node
 
@@ -79,12 +89,13 @@ class MonteCarloTree:
             False if the node is a leaf (game over)
         """
         actions = self.env.get_legal_actions()
-        if self.env.check_win_condition() or len(actions) == 0:
+        if self.env.check_win_condition():
             return False
 
         player = Player.TWO if node.player == Player.ONE else Player.ONE
         for action in actions:
-            node.children[action] = Node(node.parent, UniversalAction(action), player)
+            node.children[action] = Node(node, player)
+            node.edges[action] = [0, 0]
 
         return True
 
@@ -95,7 +106,7 @@ class MonteCarloTree:
         # Using rollout or critic
         actions = self.env.get_legal_actions()
 
-        while not self.env.check_win_condition() and actions:
+        while not self.env.check_win_condition():
             if random.uniform(0, 1) < self.epsilon:
                 action = UniversalAction(choice(actions))
             else:
@@ -107,15 +118,21 @@ class MonteCarloTree:
         return self.env.winner
 
     @staticmethod
-    def backprop(node: Node, player: Player, winner: Player) -> None:
+    def backprop(node: Node, winner: Player) -> None:
         """
         Passing the evaluation of a final state back up the tree, updating relevant data at all nodes and edges on the path from the final state to the tree root.
         """
 
-        reinforcement = 0 if winner == player else 1
-
+        reinforcement = 1 if winner == Player.ONE else -1
+        counter = 0
         while node is not None:
+            counter += 1
             node.visit_count += 1
-            node.Q_value += reinforcement
+
+            if node.cached_action:
+                action = node.cached_action.coordinates
+                node.edges[action][0] += 1
+                edge_traversals, q_value = node.edges[action]
+                node.edges[action][1] += (reinforcement - q_value) / edge_traversals
+
             node = node.parent
-            reinforcement = 0 if reinforcement == 1 else 1

src/agent/node.py
@@ -7,19 +7,21 @@ from environment.universal_state import UniversalState
 
 class Node:
 
-    def __init__(self, parent: 'Node', action: UniversalAction, player: Player) -> None:
+    def __init__(self, parent: 'Node', player: Player) -> None:
         self.parent = parent
-        self.previous_action = action
+        self.cached_action = None
         self.player = player
-        self.children = {}  # Key: node coordinate, value: Node()
+        self.children = {}  # Key: node coordinates, value: Node()
+        self.edges = {}  # Key: node coordinates, value: [traversals, q_value]
         self.visit_count = 0
-        self.Q_value = 0
 
-    """ def compute_uct(self, exploitation_factor: float) -> float:
-        exploitation_factor *= -1 if self.player == Player.TWO else 1
+    def evaluate_action(self, action: tuple, exploration_constant: float) -> float:
+        edge_traversals, q_value = self.edges[action]
+        exploration_constant *= 1 if self.player == Player.ONE else -1
 
-        if self.visit_count == 0:
-            return 0 if exploitation_factor == 0 else math.inf
-        else:
-            return self.Q_value / self.visit_count + exploitation_factor * math.sqrt(2 * math.log(self.parent.visit_count) / self.visit_count)
- """
+        # Avoid log(0)
+        node_traversals = self.visit_count if self.visit_count > 0 else 1
+        return q_value + exploration_constant * np.sqrt(np.log(node_traversals) / (1 + edge_traversals))
+
+    def __str__(self) -> str:
+        return f'cached_action: {self.cached_action}\nplayer: {self.player}\nedges: {len(self.edges)}\nvisit_count: {self.visit_count}\nparent: {True if self.parent else False}'

src/agent/reinforcement_learning.py
@@ -9,12 +9,13 @@ from agent.mcts import MonteCarloTree
 from environment.hexagonal_grid import HexagonalGrid
 from environment.universal_action import UniversalAction
 from environment.universal_state import UniversalState
+from utils.loadingbar import print_progress
 from utils.visualize_training import visualize_training
 
 
 class ReinforcementLearning:
 
-    def __init__(self, games: int, simulations: int, epochs: int, save_interval: int, epsilon: float, epsilon_decay: float, learning_rate: float, input_size: int, nn_dimensions: list, activation_functions: list) -> None:
+    def __init__(self, games: int, simulations: int, epochs: int, save_interval: int, epsilon: float, epsilon_decay: float, learning_rate: float, board_size: int, nn_dimensions: list, activation_functions: list, optimizer: str, exploration_constant: float) -> None:
         self.games = games
         self.simulations = simulations
         self.epochs = epochs
@@ -22,8 +23,9 @@ class ReinforcementLearning:
         self.epsilon = epsilon
         self.epsilon_decay = epsilon_decay
         self.save_interval = save_interval
+        self.exploration_constant = exploration_constant
 
-        self.actor = Actor(self.epochs, self.learning_rate, self.save_interval, input_size, nn_dimensions, activation_functions)
+        self.actor = Actor(self.epochs, self.learning_rate, self.save_interval, board_size, nn_dimensions, activation_functions, optimizer)
         self.replay_buffer = []
 
         self.losses = []
@@ -33,26 +35,25 @@ class ReinforcementLearning:
         env = HexagonalGrid(visual=True)
 
         init_state = UniversalState(deepcopy(env.state.nodes), env.get_player_turn())
-        tree = MonteCarloTree(deepcopy(init_state), self.actor, self.epsilon)
+        tree = MonteCarloTree(deepcopy(init_state), self.actor, self.epsilon, self.exploration_constant)
 
         for game_index in range(self.games):
             env.reset()
             tree.reset(deepcopy(init_state))
-            print(game_index, round(tree.epsilon, 3))
+
             while True:
                 # Check win condition
                 if env.check_win_condition():
-                    # print(counter)
                     #env.visualize(False, 10)
                     break
 
-                for _ in range(self.simulations):
+                for sim_index in range(self.simulations):
                     tree.single_pass()
 
-                visit_counts = tree.get_children_visit_count(tree.root)
-                self.append_replay_buffer(UniversalState(deepcopy(env.state.nodes), env.get_player_turn()), visit_counts)
+                distribution = tree.get_action_distribution(tree.root)
+                self.append_replay_buffer(UniversalState(deepcopy(env.state.nodes), env.get_player_turn()), distribution)
 
-                chosen_key = max(visit_counts, key=visit_counts.get)
+                chosen_key = max(distribution, key=distribution.get)
                 action = UniversalAction(chosen_key)
                 env.execute_action(action)
 
@@ -65,23 +66,26 @@ class ReinforcementLearning:
             self.train_actor(game_index)
             tree.epsilon *= self.epsilon_decay
 
+            loss = 0 if len(self.losses) == 0 else self.losses[-1]
+            acc = 0 if len(self.accuracies) == 0 else self.accuracies[-1]
+            print_progress(game_index, self.games, length=20, suffix=f'Epsilon: {round(tree.epsilon,5)}, Loss: {round(loss, 5)}, Acc: {round(acc,5)}')
+
+        print()
         self.actor.save_model(0)
 
         visualize_training(self.losses, self.accuracies)
 
-    def append_replay_buffer(self, state: UniversalState, visit_counts: dict) -> None:
+    def append_replay_buffer(self, state: UniversalState, distribution: dict) -> None:
 
         for (row, col) in state.nodes.keys():
-            if (row, col) in visit_counts.keys():
+            if (row, col) in distribution.keys():
                 continue
-            visit_counts[(row, col)] = 0
+            distribution[(row, col)] = 0
 
         input = state.to_numpy()
 
-        visit_count_list = np.asarray(list(visit_counts.values()))
+        visit_count_list = np.asarray(list(distribution.values()))
         target = visit_count_list / np.sum(visit_count_list)
-
-        #print(input, target)
         self.replay_buffer.append((input, target))
 
     def train_actor(self, game_index: int):